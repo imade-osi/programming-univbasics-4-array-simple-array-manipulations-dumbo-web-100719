7c4721eace822324cd68522fa65f5576a8595186
steadfast-motherboard-7317 <imadejr@yahoo.com>
2019-09-25 23:29:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,11 +23,11 @@ def shift_with_args(array)
 end 
 
 def using_concat(array1, array2)
-  array1 + array2
+  array1 = array1 + array2
 end 
 
 def using_insert(array, ele)
-  array.insert( 3, ele) 
+  array.insert(4, ele) 
 end 
 
 def using_uniq(array)
